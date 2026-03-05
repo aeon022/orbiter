6b6ba26824c6f1e3f6ab2985479cfa9c3aa7a7dc
aeon022 <gerwin.weiher@gmail.com>
2026-03-05 11:53:34 +0100

    initial commit

--- apps/demo/astro.config.mjs
+++ apps/demo/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import orbiter from '@orbiter/integration';
 
 export default defineConfig({
+  server: {
+    port: 8080,
+  },
   integrations: [
     orbiter({ pod: './demo.pod' }),
   ],
